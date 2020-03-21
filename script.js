a04e647fcf3aa8cc8dca164a860bbc85c3fb9db9
gr-olga <gr-olga@ya.ru>
2020-03-21 16:14:49 +0100

    made functionality for creating toasts dynamically

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
-function showMessage() {
-    alert('alarm!!!');
+function showMessage(title, text) {
+
+    // Создать ноду жтмл с томтом, create element/
+    // присвоить классы(для стилей)
+    // Найти body
+    // присобачить наш тост-хтмл с нашим текстом
+    document.getElementsByTagName("body")[0].appendChild(createToast(title, text))
 }
+
+function createToast(title, text) {
+    const toastEl = document.createElement('div');
+    const titleEl = document.createElement('div');
+    const textEl = document.createElement('div');
+
+    toastEl.className = 'toast';
+    titleEl.className = 'title';
+    textEl.className = 'text';
+
+    titleEl.innerText = title;
+    textEl.innerText = text;
+
+    toastEl.appendChild(titleEl);
+    toastEl.appendChild(textEl);
+
+
+
+    return toastEl;
+}
+
